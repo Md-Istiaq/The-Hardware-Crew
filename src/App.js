512aed7d7a94a8cf8984257ef12099dae8a7072f
Md-Istiaq <mdistiaq102@outlook.com>
2022-05-23 22:46:22 +0600

    registration from created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Pages/Home/Home';
 import Purchase from './Pages/Home/Parts/Purchase';
 import Login from './Pages/Login/Login';
+import SignUp from '../src/Pages/SignUp/SignUp'
 import Navbar from './Pages/Shared/Navbar';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -14,6 +15,7 @@ function App() {
        <Route path='/' element={<Home/>}></Route>
        <Route path='/home' element={<Home></Home>}></Route>
        <Route path='/login' element={<Login></Login>}></Route>
+       <Route path='/signup' element={<SignUp></SignUp>}></Route>
        <Route path='/part/:id' element={<Purchase/>}></Route>
      </Routes>
      <ToastContainer />
---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {useSignInWithGoogle} from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword ,useUpdateProfile } from 'react-firebase-hooks/auth';
+import auth from '../../_firebase.init';
+import { useForm } from "react-hook-form";
+import {Link, useNavigate} from 'react-router-dom'
+import { async } from '@firebase/util';
+const SignUp = () => {
+    const [signInWithGoogle, Guser, Gloading, Gerror] = useSignInWithGoogle(auth);
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+      const Navigate = useNavigate()
+      const [updateProfile, updating, Uerror] = useUpdateProfile(auth);
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = async data =>{ 
+        console.log(data)
+       await createUserWithEmailAndPassword(data.email,data.password)
+       await updateProfile(data.name)
+    };
+    if(Gloading || loading){
+        return <button class="btn btn-square loading">Loading</button>
+    }
+    if(Gerror || error){
+        alert(Gerror || error)
+    }
+    return (
+        <div className='LogIn hero min-h-screen bg-base-200'>
+    <form onSubmit={handleSubmit(onSubmit)} class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 LogIn-filed">
+    <h1 className='text-3xl mx-auto text-primary'>Register</h1>
+      <div class="card-body">
+      <div class="form-control">
+          <label class="label">
+            <span class="label-text text-white">Name</span>
+          </label>
+          <input type="text" placeholder="Enter you Name" class="input input-bordered LogIn-filed" {...register("name", { required: true })}  />
+        </div>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text text-white">Email</span>
+          </label>
+          <input type="text" placeholder="email" class="input input-bordered LogIn-filed" {...register("email", { required: true })}  />
+        </div>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text text-white">Password</span>
+          </label>
+          <input type="text" placeholder="password" class="input input-bordered LogIn-filed" {...register("password", { required: true })}  />
+          <label class="label">
+            <a href="#" class="label-text-alt link link-hover text-white">Forgot password?</a>
+          </label>
+        </div>
+        <p><small>Already have an account? <Link to='/login' className='link hover:text-primary'>Go to login</Link></small> </p>
+        <input type="submit" value="Sign Up" class="btn btn-primary uppercase font-bold bg-gradient-to-r from-accent to-primary hover:from-pink-500 hover:to-yellow-500 rounded-3xl hover:text-primary mt-5" />
+      </div>
+      <p>---------------------------or---------------------------</p>
+      <button onClick={() => signInWithGoogle()} class="btn btn-primary uppercase font-bold bg-gradient-to-r from-accent to-primary hover:from-pink-500 hover:to-yellow-500 rounded-3xl hover:text-primary mb-5">Continue With Google</button>
+      </form>
+      </div>
+    );
+};
+
+export default SignUp;
